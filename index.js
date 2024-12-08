5b157ee6a1107b9d5b1e86b85a1063a4073f2a23
Agent-grey <markkibuthu@gmail.com>
2024-12-08 04:36:23 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feetTravelled){
   // Write your code here!
+  if(feetTravelled <= 400){
+    return 'This one is on me!'
+  }else if(feetTravelled >= 400 && feetTravelled <= 2000){
+    return 'That will be twenty bucks.'
+  }else if(feetTravelled >= 2000 && feetTravelled <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(feetTravelled >= 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == 'NYC'? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(trip){
   // Write your code here!
+ switch(trip){
+  case "generous":
+    return "Thank you so much."
+    break;
+  case "not as generous":
+    return "Thank you."
+    break;
+  default:
+    return "Bye."
+ }
 }
